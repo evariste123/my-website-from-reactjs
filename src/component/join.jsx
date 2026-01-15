30af21202e1281ffb27cfc7f2189221cd3ac92c4
evariste123 <bapfekureraevariste@gmail.com>
2026-01-15 09:42:57 +0200

    3rd commit

--- src/component/join.jsx
+++ src/component/join.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import Header from '../assets/pages/header'
 function Join(){
     return(
-        <div>
+        <section className='join' id='join'>
         <Header/>
-        <h1>welcome to my join</h1></div>
+        <form action="">
+        <div className='sign'>
+            <h1>Get free Email updates!</h1>
+            <h4>join us for free to get instant updates</h4>
+            <input type="text" name="full name" id="firstname" required placeholder='first name'/><br /><br />
+            <input type="text" name="email" id="email" required placeholder='Email' />
+            <h4>🔒your information is safe with us!</h4>
+            <div className='butto'>
+                <button type='submit'>
+                    Get Access to day
+                </button>
+            </div>
+        </div>
+        </form>
+        </section>
     )
 }
 export default Join
