7075c663cc4a41717d1cd7899bdb015cbe155567
evariste123 <bapfekureraevariste@gmail.com>
2026-01-22 15:22:51 +0200

    10th commit

--- src/page/developer.jsx
+++ src/page/developer.jsx
@@ -1,11 +1,71 @@
 import React from "react";
 import Header from "../component1/header";
-function Developer(){
-    return(
-        <div>
-            <Header/>
-            <h1>welcome to my developer</h1>
-        </div>
+function Developer() {
+    return (
+        <section className="developer" id="developer">
+            <Header />
+            <h3>
+                this website it is designed  to help software developers learn <br />
+                 about and integrate with a product, service, or platform by providing <br />
+                  structured access to all necessary information.and also inorder to thrive your <br />
+                  products or knowledge and your skills.🤖
+            </h3>
+            <div className="sdk">
+                <div className="sdk1">
+                    <a href=""><button type="button">Call-to-Action</button></a>
+                </div>
+            </div>
+            <div className="sdk2">
+                <div className="sdk3">
+                    <a href=""><button type="button">download SDK</button></a>
+                </div>
+            </div>
+            <div className="dev">
+                <div className="devo">
+                    <a href=""><button type="button">Developer guides. <br />☝click me</button></a>
+                </div>
+            </div>
+            <section className="devi">
+                <div className="dev1">
+                    <h4>quick-links</h4>
+                    <div className="dev2">
+                        <a href="">Getting started</a>
+                        <a href="">API reference</a>
+                        <a href="">tutorial</a>
+                        <a href="">SDKs/libraries</a>
+                        <a href="">tools/resources</a>
+                        <a href="">community</a>
+                        <a href="">updates</a>
+                        <a href="">Grossary</a>
+                        <a href=""></a>
+                    </div>
+                </div>
+                <div className="dev3">
+                    <h4>home links</h4>
+                    <div className="dev4">
+                        <a href="/home">home</a>
+                        <a href="/document">document</a>
+                        <a href="/status">status</a>
+                        <a href="/support">support</a>
+                        <a href="/developer">developer</a>
+                        <a href="/agent">agent</a>
+                    </div>
+                </div>
+                <div className="dev5">
+                    <h4>social-media links</h4>
+                    <div className="dev6">
+                    <a href="https://www.youtube.com/@evaristebapfekurera5995">you tube</a>
+                    <a href="https://www.facebook.com/evariste.arrogent.ever">facebook</a>
+                    <a href="https://www.instagram.com/evari_ste2025/">instagram</a>
+                    <a href="https://www.linkedin.com/in/evariste-bapfekurera-a3700b2b0/">linkedin</a>
+                    <a href="https://x.com/EBapfekure3">twitter</a>
+                    </div>
+                </div>
+                <div className="dev7">
+                    <p>©Evariste🍁 2026</p>
+                </div>
+            </section>
+        </section>
     )
 }
 export default Developer
