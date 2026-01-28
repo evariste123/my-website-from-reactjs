e9f19a14b9b8394d00747f08e8761feda2aab3ec
evariste123 <bapfekureraevariste@gmail.com>
2026-01-28 15:17:06 +0200

    ist commit

--- src/assets/pages/header.jsx
+++ src/assets/pages/header.jsx
@@ -4,14 +4,14 @@ function Header(){
     return(
         <div className='menu'>
     <div className='bar'>
-   <a> <i className='far fa-home'></i><Link to='/'>home</Link></a>
-    <a> <i className='fas fa-heart'></i><Link to='/about'>about</Link><i className='fas fa-caret-down'></i></a>
-    <a> <i className='far fa-address-book'></i><Link to='/contact'>contact</Link></a>
-    <a> <i className='fas fa-add'></i><Link to='/product'>product</Link></a>
-    <a> <i className='fas fa-earth'></i><Link to='/service'>service</Link></a>
-    <a> <i className='fas fa-blog'></i><Link to='/blogs'>blogs</Link></a>
-    <a> <i className='far fa-user'></i><Link to='/login'>login</Link></a>
-    <a> <i className='fas fa-user'></i><Link to='/join'>join</Link></a>
+   <Link to='/'>home</Link>
+    <Link to='/about'>about</Link>
+    <Link to='/contact'>contact</Link>
+    <Link to='/product'>product</Link>
+    <Link to='/service'>service</Link>
+<Link to='/blogs'>blogs</Link>
+    <Link to='/login'>login</Link>
+    <Link to='/join'>join</Link>
         </div>
         </div>
     )
